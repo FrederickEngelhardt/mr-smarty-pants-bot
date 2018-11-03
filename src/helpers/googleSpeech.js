d5214f28ef9285c079cc8e825af89bef21dcb491
Frederick Engelhardt <fre1994@gmail.com>
2018-11-03 12:21:38 -0700

    fix: removed the useless listener function

--- src/helpers/googleSpeech.js
+++ src/helpers/googleSpeech.js
@@ -5,7 +5,7 @@ class GoogleSpeech {
   init() {
     this.listen();
   }
-  speak = (string) => {
+  speak = string => {
     let msg = new SpeechSynthesisUtterance(string);
     this.recognition.stop();
     window.speechSynthesis.speak(msg);
@@ -49,44 +49,5 @@ class GoogleSpeech {
     };
   }
 }
-
-const listen = () => {
-  let final_transcript = "";
-
-  recognition.continuous = true;
-  recognition.interimResults = true;
-  recognition.lang = "en-US";
-  recognition.start();
-  recognition.onresult = function(event) {
-    var interim_transcript = "";
-
-    if (typeof event.results === "undefined") {
-      console.log("stopping");
-      recognition.onend = null;
-      recognition.stop();
-      return;
-    }
-
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-      if (event.results[i].isFinal) {
-        final_transcript = event.results[i][0].transcript;
-        console.log(final_transcript);
-        const msg = new SpeechSynthesisUtterance(final_transcript);
-        recognition.stop();
-        window.speechSynthesis.speak(msg);
-        msg.onend = function(event) {
-          console.log(
-            "Utterance has finished being spoken after " +
-              event.elapsedTime +
-              " milliseconds."
-          );
-          recognition.start();
-        };
-      } else {
-        interim_transcript += event.results[i][0].transcript;
-      }
-    }
-  };
-};
 
 export default GoogleSpeech;

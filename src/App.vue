568de7b21e2f0ed1a68721139d1327386df90fe1
Stephen Fox <sfox@stephenfoxdev.com>
2018-11-03 14:38:29 -0700

    Add robot and speech bubble graphics

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="app-title">Mr. Smarty Pants</h1>
+    <MainChat msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainChat from './components/MainChat.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MainChat
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Orbitron', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
+  .app-title {
+    color: rgba(255,255,255, 0.8);
+    font-weight: 900;
+    font-size: 4em;
+    text-transform: uppercase;
+  }
 </style>
